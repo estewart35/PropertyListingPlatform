9a8334f087787aa6ed5c849d1947faf831846651
Ethan Stewart <ethan.stewart35@gmail.com>
2025-09-10 19:05:17 -0600

    Host media files

--- plp_project/urls.py
+++ plp_project/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
 
 
@@ -24,7 +25,8 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('plp_listings.urls')),
     path('users/', include('plp_users.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]# +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [path("admin/", admin.site.urls)]
